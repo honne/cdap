bd57db10459ba779eadbe2bd82895cc5c8faf473
Bhooshan Mogal <bhooshan@cask.co>
2016-07-30 12:05:26 -0700

    (CDAP-6833) Made the privilege fetcher in RemoteOpsService a proxy, that simply proxies list privilege requests to the master, which can communicate with authorization backends.
    - DatasetOpExecutor which runs the proxy, now also maintains an authorization cache inside PrivilegeFetcherProxyService, which is similar to the authorization enforcement cache used by program containers, except that it updates itself by fetching privileges via the master.
    - Refactored DefaultAuthorizationEnforcementService to move non-enforcement code into an AbstractAuthorizationService class, which is also used by the PrivilegesFetcherProxyService
    - Refactored Remote client and handlers for fetching privileges to move common code to abstract classes, which are extended by:
      - RemotePrivilegesFetcherHandler: runs inside master, communicates with authorization backends directly
      - RemotePrivilegesFetcherProxyHandler: runs inside RemoteSystemOpsService, proxies requests from RemotePrivilegesFetcher to RemotePrivilegesFetcherHandler, via PrivilegesFetcherProxyClient
    - (CDAP-6639) Updates to authorization cache in the master now do not go via the RemoteSystemOpsService, since master can communicate with Authorization backends directly
      - Fixed MasterAuthenticationContext to get the user id from UserGroupInformation, when requests are made to authorization backends periodically for updating master's authorization cache. Without this, the authentication context would expect SecurityRequestContext to be set, which is null when requests do not go via the Router.
    - Most logic of this change is in AuthorizationEnforcementModule.

--- cdap-app-fabric/src/test/java/co/cask/cdap/metadata/MetadataAdminAuthorizationTest.java
+++ cdap-app-fabric/src/test/java/co/cask/cdap/metadata/MetadataAdminAuthorizationTest.java
@@ -34,7 +34,6 @@ import co.cask.cdap.security.authorization.AuthorizerInstantiator;
 import co.cask.cdap.security.authorization.InMemoryAuthorizer;
 import co.cask.cdap.security.spi.authentication.SecurityRequestContext;
 import co.cask.cdap.security.spi.authorization.Authorizer;
-import co.cask.tephra.TransactionManager;
 import com.google.inject.Injector;
 import org.apache.twill.filesystem.LocalLocationFactory;
 import org.apache.twill.filesystem.Location;
